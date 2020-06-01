ccd43db7157212ffe0acbde5b63d933e4f00d175
Aaryaman Sen <54085618+aaryamansen@users.noreply.github.com>
2020-06-01 17:40:22 +0530

    CSS Grid Exploration + Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <p>Leftfield is a business development, marketing services and project delivery organization.</p>
+        <p>We assist international firms in their efforts to sell their products and services in India.</p>
+        <Link to="/page-2/">Work with us</Link>
+      </div>
+    </div>
   </div>
 )
 
